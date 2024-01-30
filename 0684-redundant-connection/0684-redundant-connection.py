f37a122d8705317db7de3c06747024adfff2f75e
Yousuff <56258703+gitifyyousuff@users.noreply.github.com>
2024-01-30 22:16:30 +0530

    Time: 58 ms (58.95%), Space: 17 MB (78.92%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -26,11 +26,12 @@ class Solution:
                 return False
             
             if rank[p1] > rank[p2]:
-                rank[p1] += rank[p2]
                 par[p2] = p1
-            else:
-                rank[p2] += rank[p1]
+            elif rank[p1] < rank[p2]:
                 par[p1] = p2
+            else:
+                par[p1] = p2
+                rank[p1] > rank[p2]
                 
             return True
         

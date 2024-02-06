6fbdb0b0f784581ffe28f9b62ce366d52a966d2e
Yousuff <56258703+gitifyyousuff@users.noreply.github.com>
2024-02-07 00:10:59 +0530

    Time: 271 ms (83.99%), Space: 17.8 MB (75.68%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,34 +1,44 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        #Floyd warshel Algorithm
+        dist = [[float('inf')]*n for _ in range(n)]
         
-        dist = [[math.inf] * n for _ in range(n)]
-
-        for node in range(n):
-            dist[node][node] = 0
-        for source, dest, weight in edges:
-            dist[source][dest] = weight
-            dist[dest][source] = weight
-
-        for pivot in range(n):
-            for nodea in range(n):
-                for nodeb in range(n):
-                    trans_dist = dist[nodea][pivot] + dist[pivot][nodeb]
-                    if trans_dist < dist[nodea][nodeb]:
-                        dist[nodea][nodeb] = trans_dist
-                        dist[nodeb][nodea] = trans_dist
-
-        min_city, min_reach = None, math.inf
-        for nodea in range(n):
-            nodea_reach = 0
-            for nodeb in range(n):
-                if dist[nodea][nodeb] <= distanceThreshold:
-                    nodea_reach += 1
-            if nodea_reach <= min_reach:
-                min_city = nodea
-                min_reach = nodea_reach
-
+        #making diagonal cell as 0
+        for i in range(n):
+            dist[i][i] = 0
+                    
+        #making direct connection weight marking for adjacency matrix 
+        #s-source, d-destination, w-weight
+        for s,d,w in edges:
+            dist[s][d] = w
+            dist[d][s] = w
+            
+        #making shortest possible distance on each edges to vertices 
+        #mark it in adjacency matrix by floyd warshel formula
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    trans_dist = dist[i][k] + dist[k][j]
+                    if dist[i][j] > trans_dist:
+                        dist[i][j] = trans_dist
+                        dist[j][i] = trans_dist
+                        
+                        
+        #main logic
+        min_city, min_reach = None,float('inf')
+        for node_a in range(n):
+            temp_reach = 0
+            for node_b in range(n):
+                if dist[node_a][node_b] <= distanceThreshold:
+                    temp_reach += 1
+            if temp_reach <= min_reach:  
+                min_city = node_a
+                min_reach = temp_reach
+                
+                
+        
         return min_city
-
+            
                     
                     
                     

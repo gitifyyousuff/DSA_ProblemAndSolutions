aebbdcac961bf4a12752a3ef64a8f2637ee82357
Yousuff <56258703+gitifyyousuff@users.noreply.github.com>
2024-02-08 23:02:40 +0530

    Time: 121 ms (91.27%), Space: 17.2 MB (90.06%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,49 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        #union-find Algorithm
+        par = [i for i in range(len(stones)+1)]
+        rank = [0] * (len(stones)+1)
+        
+        #find function
+        def find(i):
+            if i == par[i]:
+                return i
+            else:
+                return find(par[i])
+            
+        #union function
+        def union(p1,p2):
+            x1,x2 = find(p1),find(p2)
+            
+            if x1 == x2:
+                return False
+            
+            if rank[x1]>rank[x2]:
+                par[x2] = x1
+            elif rank[x1]<rank[x2]:
+                par[x1] = x2
+            else:
+                par[x1] = x2
+                rank[x2] += 1
+                
+            return True
+                
+        
+        rows, cols = {}, {}
+        removed = 0
+        for i, (row, col) in enumerate(stones):
+            if row in rows:
+                removed += union(i, rows[row])
+            else:
+                rows[row] = i
+            if col in cols:
+                removed += union(i, cols[col])
+            else:
+                cols[col] = i
+
+        return removed
+                
+                
+            
+        
+        

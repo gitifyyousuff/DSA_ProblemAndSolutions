96997e5b7e1857e77eebf7c5487fbbb30e5d07eb
Yousuff <56258703+gitifyyousuff@users.noreply.github.com>
2024-02-09 16:02:47 +0530

    Time: 114 ms (96.86%), Space: 17.2 MB (82.36%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -1,8 +1,9 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
         #union-find Algorithm
-        par = [i for i in range(len(stones)+1)]
-        rank = [0] * (len(stones)+1)
+        n =len(stones)
+        par = [i for i in range(n)]
+        rank = [0] * n
         
         #find function
         def find(i):
@@ -12,37 +13,37 @@ class Solution:
                 return find(par[i])
             
         #union function
-        def union(p1,p2):
-            x1,x2 = find(p1),find(p2)
-            
+        def union(p1, p2):
+            x1, x2 = find(p1), find(p2)
+
             if x1 == x2:
                 return False
-            
-            if rank[x1]>rank[x2]:
+
+            if rank[x1] > rank[x2]:
                 par[x2] = x1
-            elif rank[x1]<rank[x2]:
+            elif rank[x1] < rank[x2]:
                 par[x1] = x2
             else:
                 par[x1] = x2
-                rank[x2] += 1
-                
+                rank[x2] += 1  # Increment rank of x2
             return True
                 
         
-        rows, cols = {}, {}
-        removed = 0
-        for i, (row, col) in enumerate(stones):
+        #logic for this problem
+        rows,cols ={},{}
+        removed = 0 
+        for i, (row,col) in enumerate(stones):
             if row in rows:
-                removed += union(i, rows[row])
+                removed += union(i,rows[row])
             else:
                 rows[row] = i
+                
             if col in cols:
-                removed += union(i, cols[col])
+                removed += union(i,cols[col])
             else:
                 cols[col] = i
-
-        return removed
                 
+        return removed
                 
             
         

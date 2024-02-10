8f940f6132614deac05f9945b8181e4285db008c
Yousuff <56258703+gitifyyousuff@users.noreply.github.com>
2024-02-10 13:50:49 +0530

    Time: 407 ms (46.86%), Space: 40.3 MB (16.92%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -4,51 +4,77 @@ class Solution:
         if n-1 > len(connections):
             return -1
         
-        #union-find algorithm
-        par = [i for i in range(len(connections)+1)]
-        rank = [0] * (len(connections)+1)
+#         #union-find algorithm
+#         par = [i for i in range(len(connections)+1)]
+#         rank = [0] * (len(connections)+1)
         
-        def find(n):
-            if n == par[n]:
-                return n
-            else:
-                return find(par[n])
+#         def find(n):
+#             if n == par[n]:
+#                 return n
+#             else:
+#                 return find(par[n])
         
-        #n1-node1,n2-node2 respectively
-        def union(n1,n2):
-            p1,p2 = find(n1),find(n2)
+#         #n1-node1,n2-node2 respectively
+#         def union(n1,n2):
+#             p1,p2 = find(n1),find(n2)
             
-            if p1 == p2 :
+#             if p1 == p2 :
+#                 return False
+            
+#             if rank[p1] > rank[p2]:
+#                 par[p2] = p1
+#             elif rank[p1] < rank[p2]:
+#                 par[p1] = p2
+#             else:
+#                 par[p1] = p2
+#                 rank[p2] += 1
+                
+#             return True
+        
+#         #main logic
+        
+        
+#         #finding extra_cable else connecting with n/w
+#         extra_cable = 0
+#         for x,y in connections:
+#             if find(x) == find(y):
+#                 extra_cable += 1
+#             else:
+#                 union(x,y)
+                
+#         #counting components and getting result
+#         component = 0 
+#         for i in range(n):
+#             if par[i] == i:
+#                 component += 1
+                
+#         return component-1
+
+        #DFS
+        graph = [set() for i in range(n)]
+        visited = [False] *n
+        
+        #1. adjacency matrix
+        for src,destn in connections:
+            graph[src].add(destn)
+            graph[destn].add(src)
+            
+        #2.DFS algo
+        def dfs(node):
+            #base case
+            if visited[node]:
                 return False
             
-            if rank[p1] > rank[p2]:
-                par[p2] = p1
-            elif rank[p1] < rank[p2]:
-                par[p1] = p2
-            else:
-                par[p1] = p2
-                rank[p2] += 1
+            visited[node] = True
+            
+            for neighbour in graph[node]:
+                dfs(neighbour)
                 
             return True
         
-        #main logic
-        
-        
-        #finding extra_cable else connecting with n/w
-        extra_cable = 0
-        for x,y in connections:
-            if find(x) == find(y):
-                extra_cable += 1
-            else:
-                union(x,y)
+        #calculation
+        return sum(dfs(node) for node in range(n))-1
                 
-        #counting components and getting result
-        component = 0 
-        for i in range(n):
-            if par[i] == i:
-                component += 1
-                
-        return component-1
         
                              
          

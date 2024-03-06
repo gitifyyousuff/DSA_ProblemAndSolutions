86178e9f214e00d236641f2929b67040c8ab8e10
Yousuff <56258703+gitifyyousuff@users.noreply.github.com>
2024-03-07 00:24:00 +0530

    Time: 455 ms (31.83%), Space: 172.4 MB (6.99%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -2,24 +2,40 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         #top-bottom approach
         #memoization
-        cache = {}
+#         cache = {}
         
+#         def dfs(i,j):
+#             #Base case 
+#             if j == len(t):
+#                 return 1
+#             if i == len(s):
+#                 return 0          
+#             if (i,j) in cache:
+#                 return cache[(i,j)]
+            
+#             if s[i] == t[j]:
+#                 cache[(i,j)] = dfs(i+1,j+1) + dfs(i+1,j)
+#             else:
+#                 cache[(i,j)] = dfs(i+1,j)
+                
+#             return cache[(i,j)]
+        
+#         return dfs(0,0)
+            
+        
+        @cache
         def dfs(i,j):
             #Base case 
             if j == len(t):
                 return 1
             if i == len(s):
                 return 0          
-            if (i,j) in cache:
-                return cache[(i,j)]
-            
+   
+            res = dfs(i+1,j)
             if s[i] == t[j]:
-                cache[(i,j)] = dfs(i+1,j+1) + dfs(i+1,j)
-            else:
-                cache[(i,j)] = dfs(i+1,j)
-                
-            return cache[(i,j)]
-        
+                res += dfs(i+1,j+1)
+            return res
+
         return dfs(0,0)
             
             

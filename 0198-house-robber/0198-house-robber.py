4860ee5ca03689dfae716ec9446e3f06429addef
Yousuff <56258703+gitifyyousuff@users.noreply.github.com>
2024-03-14 23:16:40 +0530

    Time: 48 ms (5.03%), Space: 16.4 MB (99.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,29 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        n = len(nums)
+        if n == 1:
             return nums[0]
+        if n == 2:
+            return max(nums)
         
-        if len(nums) == 2:
-            return nums[0] if nums[0] > nums[1] else nums[1]
-
-        def dp(i, nums, cache):
-            # base case
-            if i >= len(nums)-2 and i < len(nums):
-                return nums[i]
-            if i >= len(nums)-1:
-                return 0
-            # cache
-            if i in cache:
-                return cache[i]
-
-            rob_next_2 = dp(i+2, nums, cache)
-            rob_next_3 = dp(i+3, nums, cache)
-
-            max_n = max(rob_next_2, rob_next_3)
-            cache[i] = max_n + nums[i]
-
-            return max_n + nums[i]
-
-        cache = {}
+        running_max = [nums[0],max(nums[0],nums[1])]
         
-        return max(dp(0, nums, cache), dp(1, nums, cache))
+        for i in range(2,n):
+            curr_val = max(nums[i]+running_max[i-2],running_max[i-1])
+            running_max.append(curr_val)
+            
+        return running_max[-1]

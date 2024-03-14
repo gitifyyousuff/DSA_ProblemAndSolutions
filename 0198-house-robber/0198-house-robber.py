7ecf6e8bb17f77de960ab40aacbf01790e5667f3
Yousuff <56258703+gitifyyousuff@users.noreply.github.com>
2024-03-14 23:47:24 +0530

    Time: 39 ms (33.86%), Space: 16.5 MB (90.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1:
-            return nums[0]
         
-        running_max = [nums[0],max(nums[0],nums[1])]
+        #bottom-up approach - O(n)TS
+#         n = len(nums)
+#         if n == 1:
+#             return nums[0]
         
-        for i in range(2,n):
-            curr_val = max(nums[i]+running_max[i-2],running_max[i-1])
-            running_max.append(curr_val)
+#         running_max = [nums[0],max(nums[0],nums[1])]
+        
+#         for i in range(2,n):
+#             curr_val = max(nums[i]+running_max[i-2],running_max[i-1])
+#             running_max.append(curr_val)
             
-        return running_max[-1]
+#         return running_max[-1]
+
+        #O(n)T,O(1)S
+        loot1,loot2 = 0,0
+        
+        for v in nums:
+            temp = max(loot1+v,loot2)
+            loot1 = loot2
+            loot2 = temp
+            
+        return loot2
+    

7642d55dc5c44e62cadccef21415137d8257ad68
Yousuff <56258703+gitifyyousuff@users.noreply.github.com>
2024-03-25 23:38:02 +0530

    Time: 61 ms (94.07%), Space: 16.9 MB (44.41%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,17 +1,27 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         #1 because min 1 length(itself a subsequence) to max of length of nums
-        dp = [1] * (len(nums))
+#         dp = [1] * (len(nums))
         
-        #Bottom-up approach -O(n^2)
-        #traversing in a reverse order 
-        for i in range(len(nums)-1,-1,-1):
-            #since it's subsequence, need to check i with all the possible val till start
-            for j in range(i+1,len(nums)):
-                #below condtion makes sure, it is in increasing order
-                if nums[i] < nums[j]:
-                    dp[i] = max(dp[i],1+dp[j])
+#         #Bottom-up approach -O(n^2)
+#         #traversing in a reverse order 
+#         for i in range(len(nums)-1,-1,-1):
+#             #since it's subsequence, need to check i with all the possible val till start
+#             for j in range(i+1,len(nums)):
+#                 #below condtion makes sure, it is in increasing order
+#                 if nums[i] < nums[j]:
+#                     dp[i] = max(dp[i],1+dp[j])
                     
-        return max(dp)
+#         return max(dp)
                     
+        dp =[]
+        for i in nums:
+            idx = bisect.bisect_left(dp,i)
+            
+            if idx < len(dp):
+                dp[idx] = i
+            else:
+                dp.append(i)
+                
+        return len(dp)
         
